caea0baba38d0b78ebbc06cf691b701d0ac7047f
vikrant bhadauria <vikrantbhadauriya2@gmail.com>
2019-11-13 20:55:50 +0530

    Add files via upload

---
+++ 13Nov2019/demo1.cpp
@@ -0,0 +1,25 @@
+#include<iostream> // header files.
+using namespace std; //namespace usage.
+
+//Main Function
+int main(){
+    //Variable Declaration
+    // datatype -> variable_name(Identifiers) ->;
+    int a;
+
+    //Variable Declaration with Initialization.
+    int b = 78;
+    a = 12;
+
+    //printing on console(black output window).
+    cout<<b<<" and "<<a<<endl;
+
+
+
+    return 0;
+}
+
+
+
+//Keywords->return,using,include,namespace,int.
+//main->Top Level Function which executes first.
---
+++ 13Nov2019/demo2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // std::cout<<"Hello World"<<std::endl;
+    int a = 0;//garbage Value.
+
+    cout<<"Enter a number"<<endl;
+    cin>>a;
+
+    cout<<"Value of A is "<<a<<endl;
+
+    return 0;
+}
+
+
+//:: ->Scope Resolution.
+
+
+//int,float,double,char,string;
